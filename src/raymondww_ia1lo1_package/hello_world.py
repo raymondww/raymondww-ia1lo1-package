ee31ec1ffe84f83fdd5a728f903f49f845fdea8c
rw1030 <rw1030@student.ubc.ca>
2026-01-06 21:03:37 -0800

    change name

---
+++ src/raymondww_ia1lo1_package/hello_world.py
@@ -0,0 +1,14 @@
+def hello_world(name: str = "World") -> str:
+    """Return a greeting message. 
+    
+    Parameters
+    ----------
+    name : str
+        The name to greet
+        
+    Returns
+    -------
+    str
+        A greeting message
+    """
+    return f"Hello, {name}!"
